5a962c7d275934d74c918769035f85f15f2e1bf7
Gabriel Solomon <hello@gabrielsolomon.ro>
2024-11-27 19:30:43 +0200

    refactor: add php 8.2 and 8.3

--- src/Utility/PhpBinary.php
+++ src/Utility/PhpBinary.php
@@ -12,6 +12,9 @@ class PhpBinary
     public const PHP_7_4 = '/usr/bin/php7.4';
     public const PHP_8_0 = '/usr/bin/php8.0';
     public const PHP_8_1 = '/usr/bin/php8.1';
+    public const PHP_8_2 = '/usr/bin/php8.2';
+    public const PHP_8_3 = '/usr/bin/php8.3';
+    public const PHP_8_4 = '/usr/bin/php8.4';
 
     /**
      * @param $bin
